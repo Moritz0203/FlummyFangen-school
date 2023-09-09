939ff7bd58e728f88e5284095e39b794efa625ef
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-09-09 16:16:24 +0200

    Add project files.

---
+++ FlummyFangen/Flummy.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FlummyFangen
+{
+    class Flummy
+    {
+        Ellipse ball;
+        Grid feld;
+        double x, y;
+        double dx = 5, dy = 5;
+        bool nachRechts = true;
+        bool nachUnten = true;
+        SolidColorBrush Füllung;
+
+        public Flummy(Grid Spielfeld, double Left, double Top, bool Richtung, bool Oben)
+        {
+            nachRechts = Richtung;
+            nachUnten = Oben;
+            x = Left;
+            y = Top;
+
+            Füllung = new SolidColorBrush();
+            Füllung.Color = Colors.Green;
+            ball = new Ellipse();
+
+            ball.Fill = Füllung;
+            ball.VerticalAlignment = VerticalAlignment.Top;
+            ball.HorizontalAlignment = HorizontalAlignment.Left;
+
+            feld = Spielfeld;
+            feld.Margin = new Thickness(5, 5, 5, 5);
+
+            feld.Children.Add(ball);
+
+            ball.Height = 50;
+            ball.Width = 50;
+            ball.Margin = new Thickness(x, y, 0, 0);
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+
+            // Fügen Sie das MouseLeftButtonDown-Ereignis hinzu
+            ball.MouseLeftButtonDown += Ball_MouseLeftButtonDown;
+        }
+
+        public void SetSpeed(double speed)
+        {
+            // Setze die Geschwindigkeit basierend auf dem übergebenen Wert
+            dx = speed;
+            dy = speed;
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            if (nachRechts)
+            {
+                x += dx;
+            }
+            else
+            {
+                x -= dx;
+            }
+
+            if (nachUnten)
+            {
+                y += dy;
+            }
+            else
+            {
+                y -= dy;
+            }
+
+            // Überprüfe Kollisionen mit den Spielfeldgrenzen
+            if (x <= 0 || x + ball.Width >= feld.ActualWidth)
+            {
+                nachRechts = !nachRechts;
+            }
+
+            if (y <= 0 || y + ball.Height >= feld.ActualHeight)
+            {
+                nachUnten = !nachUnten;
+            }
+
+            // Aktualisiere die Position des Flummis
+            ball.Margin = new Thickness(x, y, 0, 0);
+        }
+
+        private void Ball_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Ändere die Farbe auf Rot
+            Füllung.Color = Colors.Red;
+
+            dx += 1;
+            dy += 1;
+
+            // Starten Sie einen Timer, um die Farbe zurückzusetzen und die Geschwindigkeit zu reduzieren
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000); // 1 Sekunde
+            timer.Tick += (s, args) =>
+            {
+                Füllung.Color = Colors.Green;
+                timer.Stop();
+            };
+
+            timer.Start();
+        }
+    }
+}
---
+++ FlummyFangen/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlummyFangen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Flummy flummyBall;
+        UInt16 GameBals = 0;
+        List<Flummy> flummies = new List<Flummy>();
+        Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            CreateBals();
+            button1.IsEnabled = false;
+            SliderBals.IsEnabled = false;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SolidColorBrush Füllung = new SolidColorBrush();
+            Füllung.Color = Colors.Gray;
+            if (flummyBall != null)
+            {
+                double speed = SliderVelo.Value; // Geschwindigkeit aus dem Slider-Wert erhalten
+                foreach (Flummy flummy in flummies)
+                {
+                    flummy.SetSpeed(speed); // Setze die Geschwindigkeit für die Flummy-Instanz
+                }
+            }
+            Spielfeld.Background = Füllung;
+        }
+
+        private void SliderBals_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            GameBals = (UInt16)SliderBals.Value;
+        }
+
+        private async void CreateBals()
+        {
+
+            UInt16 Left = 10;
+            UInt16 Top = 20;
+            for (UInt16 i = 0; i <= GameBals; i++)
+            {
+                flummyBall = new Flummy(Spielfeld, Left, Top, RandomBool(), RandomBool());
+                flummies.Add(flummyBall);
+
+                Left += (UInt16)random.Next(20, 120);
+                Top += 15;
+                await Task.Delay(500);
+            }
+        }
+
+        public bool RandomBool()
+        {
+            return random.Next(2) == 0;
+        }
+    }
+
+    
+}
